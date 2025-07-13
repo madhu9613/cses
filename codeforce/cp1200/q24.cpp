0088267181f96eaacb12c37ef0ff4466d7f1a7fb
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-13 20:58:28 +0530

    cp-24th qyeatuin

---
+++ codeforce/cp1200/q24.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n;
+        cin>>n;
+
+        string s;
+        cin>>s;
+
+        int count0 = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '0') count0++;
+        } //? O(N)
+
+        if((count0 % 2 == 0) or (count0 == 1)) {
+            cout<<"BOB\n";
+        }
+        else {
+            cout<<"ALICE\n";
+        }   
+    }
+
+    return 0;
+}
