4e1d0a9e0bcd4b80e5fbf77dd5a0192fc20ed892
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-15 18:09:05 +0530

    range query

--- rangequeries/minqueue2.cpp
+++ rangequeries/minqueue2.cpp
@@ -1,46 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-int mininrange(int l, int u, vector<int> arr)
+vector<int> segTree;
+vector<int> arr;
+
+void BuildTree(int node, int l, int r)
 {
-    int min =arr[l];
-    for(int i=l;i<=u;i++)
-    {
-         if(arr[i]<min)
-         {
-            min=arr[i];
-         }
-    }
-    return min;
+   if (l == r)
+   {
+      segTree[node] = arr[l];
+   }
+   else
+   {
+      int mid = l + (r - l) / 2;
+      BuildTree(2 * node, l, mid);
+      BuildTree(2 * node + 1, mid + 1, r);
+      segTree[node] = min(segTree[2 * node], segTree[2 * node + 1]);
+   }
 }
-int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> arr;
-    vector<int> result;
-    for(int i=0;i<n;i++)
-    {
-           int d;
-           cin>>d;
-           arr.push_back(d);
 
-    }
-    for(int i=0;i<q;i++)
-    {
+int query(int node, int l, int r, int ql, int qr)
+{
+   if (ql > r || qr < l)
+      return INT_MAX; // here its out of bound
+   if (ql <= l && r <= qr)
+   {
+      // conpletly overlapping
+      return segTree[node];
+   }
+   int mid = l + (r - l) / 2;
+   return min(query(2 * node, l, mid, ql, qr),
+              query(2 * node + 1, mid + 1, r, ql, qr));
+}
 
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int n, q;
+   cin >> n >> q;
+   segTree.resize(4 * n);
+   arr.resize(n + 1);
+   for (int i = 1; i <= n; i++)
+   {
+      cin >> arr[i];
+   }
 
-       int a,b;
-       cin>>a>>b;
-       a--,b--;
-       int ans=mininrange(a,b,arr);
-       result.push_back(ans);
-      
-    }
- for(int i: result)
- {
-    cout<<i<<endl;
- }
-return 0;
-    
-    }
-
- 
+   BuildTree(1,1,n);
+   while (q--)
+   {
+      int a,b;
+      cin>>a>>b;
+      cout<<query(1,1,n,a,b)<<"\n";
+   }
+   
+   return 0;
+}
