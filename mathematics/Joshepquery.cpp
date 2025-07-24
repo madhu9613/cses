ec4ffc0b7c30d913f695d9dfa130df9b2cf419a1
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-25 00:09:23 +0530

    joshepquery ad '

---
+++ mathematics/Joshepquery.cpp
@@ -0,0 +1,40 @@
+// Author: Madhujya Rajkhowa
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+ll f(ll n,ll k)
+{
+    if(n==1) return 1;
+    if(k<=(n+1)/2) 
+    {
+        if(2*k>n) return (2*k)%n;
+        else return 2*k;
+    }
+    ll temp=f(n/2,k-(n+1)/2);
+    if(n%2==1) return 2*temp+1;
+    return 2*temp-1;
+}
+void solve() {
+    
+        ll n,k;
+        cin>>n>>k;
+        cout<<f(n,k)<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
--- mathematics/sumofdivisor.cpp
+++ mathematics/sumofdivisor.cpp
@@ -1,33 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define int long long
 const int MOD = 1e9 + 7;
 
-// Function to compute the sum of divisors sum(σ(i)) efficiently
-int sum_of_divisors(int n) {
-    int total = 0;
-    
-    for (int i = 1; i * i <= n; i++) {
-        int quotient = n / i;
-        int next_i = n / quotient + 1;  // The next distinct quotient
-        
-        // Contribution of the range [i, next_i - 1]
-        int sum_range = (quotient % MOD) * ((next_i - i) % MOD) % MOD;
-        total = (total + sum_range) % MOD;
-        
-        i = next_i - 1; // Move to the next unique divisor
-    }
-
-    return total;
+// Sum of first k numbers: 1 + 2 + ... + k
+int sum_upto(int k) {
+    k %= MOD;
+    return (k * (k + 1) / 2) % MOD;
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
     int n;
     cin >> n;
-    cout << sum_of_divisors(n) << '\n';
 
+    int ans = 0;
+    int i = 1;
+
+    while (i <= n) {
+        int val = n / i;
+        int j = n / val;
+
+        // i to j all have floor(n/d) = val
+        int sum_d = (sum_upto(j) - sum_upto(i - 1) + MOD) % MOD;
+
+        ans = (ans + val % MOD * sum_d % MOD) % MOD;
+
+        i = j + 1;
+    }
+
+    cout << ans << '\n';
     return 0;
 }
