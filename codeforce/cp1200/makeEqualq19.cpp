1e6ca2d0453eed4c90d4157bc3c22f03a89d0714
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-25 00:10:05 +0530

    cp rating wise|

--- codeforce/cp1200/makeEqualq19.cpp
+++ codeforce/cp1200/makeEqualq19.cpp
@@ -1,62 +1,79 @@
-#include<bits/stdc++.h>
+// Author: Madhujya Rajkhowa
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+
+void solve()
 {
-    int t;cin>>t;
-    while (t--)
+    int n;
+    char c;
+    cin >> n >> c;
+    string s;
+    cin >> s;
+    bool isallsame = true;
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        char c;
-        string s;
-        cin>>n>>c>>s;
-        bool isOdd=false;
-        int max_even=0;
-        for(int i=0;i<n;i++)
+        if (s[i] != c)
         {
-            if(s[i]!=c)
-            {
-                if((i+1)%2==0)
-                 {
-                    max_even=max(max_even,i+1);
-                 }
-                 else{
-                    isOdd=true;
-                 }                
-            }
-        }
-        if(max_even==0 && !isOdd)
-        {
-            cout<<0<<endl;
-        }
-        else if(max_even==0 && isOdd)
-        {
-            cout<<1<<endl;
-            cout<<2<<endl;
-        }
-        else if(max_even!=0 && !isOdd)
-        {
-            cout<<1<<endl;
-            if(max_even/2%2==0)
-            {
-                cout<<max_even/2+1<<endl;
-            }
-            else{
-                cout<<max_even/2+2<<endl;
-            }
-        }
-        else{
-            cout<<2<<endl;
-            cout<<2<<" ";
-                 if(max_even/2%2==0)
-            {
-                cout<<max_even/2+1<<endl;
-            }
-            else{
-                cout<<max_even/2+2<<endl;
-            }
+            isallsame = false;
+            break;
         }
     }
+    if (isallsame)
+    {
+        cout << 0 << endl;
+        return;
+    }
 
+    //check fr 1 operation
+
+    int idx = -1;
+    for (int i = 2; i <= n; i++)
+    {
+        bool isPoss = true;
+        for (int j = i; j <= n; j += i)
+        {
+            if (s[j - 1] != c)
+            {
+                isPoss = false;
+                break;
+            }
+        }
+
+        if (isPoss)
+        {
+            idx = i;
+            break;
+        }
+    }
+    if(idx!=-1)
+    {
+        cout<<1<<endl;
+        cout<<idx<<endl;
+        return;
+    }
+
+    cout<<2<<endl;
+    cout<<n-1<<" "<<n<<endl;
     
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
