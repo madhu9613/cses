583a461dd4f2ded4dc340ccd09592578c1ae743c
Madhujya Rajkhowa <rajkhowamadhujya0@gmail.com>
2025-07-26 09:54:26 +0530

    Adding from mobile

---
+++ Cses1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 50005;
+
+int n, m;
+vector<int> rev_adj[MAXN]; // reverse graph
+vector<int> out_deg(MAXN, 0);
+bitset<MAXN> reachable[MAXN];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);          // original graph
+        rev_adj[b].push_back(a);      // reverse graph
+        out_deg[a]++;
+    }
+
+    queue<int> q;
+    // Start from nodes with 0 out-degree (in original graph)
+    for (int i = 1; i <= n; ++i) {
+        if (out_deg[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        // A node can always reach itself
+        reachable[u][u] = 1;
+
+        for (int v : rev_adj[u]) { // for each parent
+            reachable[v] |= reachable[u]; // merge reachable sets
+            out_deg[v]--;
+            if (out_deg[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        cout << reachable[i].count() << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
