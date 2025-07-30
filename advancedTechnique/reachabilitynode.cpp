e215e0497a296c51d1007fba6670758ad10cd266
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-30 11:31:50 +0530

    modifying

--- advancedTechnique/reachabilitynode.cpp
+++ advancedTechnique/reachabilitynode.cpp
@@ -1,6 +1,3 @@
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,23 +12,23 @@ const int MOD = 1e9 + 7;
 const int INF = INT_MAX;
 
 const int MAXN = 50005;
-
+ 
 int n, m;
 vector<int> rev_adj[MAXN]; 
 vector<int> out_deg(MAXN, 0);
 bitset<MAXN> reachable[MAXN];
 
 void solve() {
-    cin >> n >> m;
+   cin >> n >> m;
     vector<int> adj[n + 1];
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
-        adj[a].push_back(b);          // original graph
-        rev_adj[b].push_back(a);      // reverse graph
+        adj[a].push_back(b);         
+        rev_adj[b].push_back(a);     
         out_deg[a]++;
     }
-
+ 
     queue<int> q;
     // Start from nodes with 0 out-degree (in original graph)
     for (int i = 1; i <= n; ++i) {
@@ -39,12 +36,12 @@ void solve() {
             q.push(i);
         }
     }
-
+ 
     while (!q.empty()) {
         int u = q.front(); q.pop();
         // A node can always reach itself
         reachable[u][u] = 1;
-
+ 
         for (int v : rev_adj[u]) { // for each parent
             reachable[v] |= reachable[u]; // merge reachable sets
             out_deg[v]--;
@@ -53,11 +50,12 @@ void solve() {
             }
         }
     }
-
+ 
     for (int i = 1; i <= n; ++i) {
         cout << reachable[i].count() << " ";
     }
     cout << "\n";
+
 
 }
 
@@ -65,7 +63,6 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int t = 1;
-    cin >> t;
     while (t--) solve();
     return 0;
 }
